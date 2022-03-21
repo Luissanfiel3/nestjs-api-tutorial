dec7bdf11fe2152f78524e7fc9aed1ab4a87bd00
Luis José Sanfiel <lsanfiel@integracanarias.com>
2022-03-21 15:59:52 +0000

    prism migration and independent service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   singup() {
     return { msg: 'Ihave signed up' };
   }
   signin() {
-    return { msg: 'Ihave signed in' };;
+    return { msg: 'Ihave signed in' };
   }
 }
 
